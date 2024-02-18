2f8fc6d5c7999a63a1e6a9afb18e31da474d9b69
Deep Raval <60513881+deep-18@users.noreply.github.com>
2024-02-18 00:33:28 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCSrKqa8vmB2V8sKlT11do_iTBBuQrAt5w",
-    authDomain: "furniture-ecomm-website-49470.firebaseapp.com",
-    databaseURL: "https://furniture-ecomm-website-49470-default-rtdb.firebaseio.com",
-    projectId: "furniture-ecomm-website-49470",
-    storageBucket: "furniture-ecomm-website-49470.appspot.com",
-    messagingSenderId: "450579094036",
-    appId: "1:450579094036:web:107c37ab7828e12778e501",
-    measurementId: "G-M4F6D9YG6V"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 
